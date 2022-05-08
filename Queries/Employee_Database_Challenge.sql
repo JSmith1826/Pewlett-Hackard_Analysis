cde441fcd3ea2b8f4c3bf632cf75aa511d9bdbbb
unknown <JSmith1826@github.com>
2022-05-08 14:17:12 -0400

    Deliv 3 completed

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -15,14 +15,15 @@ t.from_date,
 t.to_date
 into retirement_titles
 from employees as e
-inner join titles as t
-on e.emp_no = t.emp_no
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31');
+    inner join titles as t
+        on e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no ASC;
 
 select * from retirement_titles
 
 
--- Create clean retirement list, removed dups
+-- Create clean retirement list, removing dups
 SELECT DISTINCT ON (r.emp_no) r.emp_no,
 r.first_name,
 r.last_name,
@@ -33,33 +34,39 @@ FROM retirement_titles as r
 WHERE to_date = '9999-01-01'
 ORDER BY emp_no ASC;
 
--- 
+-- Ops
 select * from unique_titles
 
 drop table unique_titles
 
--- Count the number of positions open by title
-select count(title), title 
-from retirement_titles
+-- Count the number of positions open by title (Deliverable 1)
+select count(u.emp_no), u.title 
+into retirement_titles
+from unique_titles as u
 group by title
 order by count(title) desc;
 
 
 drop table mentor
 
---Create Mentor Table 
+--Create Mentor Table (Deliverable 2)
 select DISTINCT on (e.emp_no)
-e.emp_no, e.first_name, e.last_name, e.birth_date,
-d.from_date, d.to_date,
+e.emp_no, 
+e.first_name, 
+e.last_name, 
+e.birth_date,
+d.from_date, 
+d.to_date,
 t.title
+
 into mentor
 from employees as e
-inner join titles as t 
-on e.emp_no = t.emp_no
-inner join dept_emp as d 
-on e.emp_no = d.emp_no
+    inner join titles as t 
+        on e.emp_no = t.emp_no
+    inner join dept_emp as d 
+        on e.emp_no = d.emp_no
 WHERE d.to_date = '9999-01-01' 
 AND (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
-ORDER BY e.emp_no asc
+ORDER BY e.emp_no asc;
 
 select * from mentor
